b46d1ff27320b48944d5c39fa48eead8507e8795
Cheng Liang <149848425+15879414325@users.noreply.github.com>
2025-11-04 11:15:02 +0800

    Update trans.py

--- flash_dry/trans.py
+++ flash_dry/trans.py
@@ -4,7 +4,7 @@ Created on Wed Aug  7 14:46:24 2024
 
 @author: 33501
 
-此代码将3个不同深度的土壤数据合并成一个土壤数据
+This code combines three sets of soil data of different depths into one soil data set.
 """
 
 
@@ -12,14 +12,14 @@ import xarray as xr
 import os
 # import merge
 
-#此方法采用加权法，下列三个参数为三个土壤层权重
+#This method employs the weighting method. The following three parameters represent the weights of the three soil layers.
 c1=0.2333
 c2=0.7
 c3=0.0667
 
 path_head = r'D:\work\code\yuan\data\soil_moisture\Nc\hist'
 
-#研究时间段
+#Research period
 start_year = 1980
 end_year = 2023
 
@@ -32,11 +32,12 @@ for year in range(start_year,end_year+1):
     swvl2 = data['swvl2'].values
     swvl3 = data['swvl3'].values
 
-    swvl30 = swvl1*c1+swvl2*c2+swvl3*c3  #合并土壤数据
+    swvl30 = swvl1*c1+swvl2*c2+swvl3*c3  #Merge soil data
     
     out1 = xr.Dataset({'swvl30':(['time','lat','lon'],swvl30)},{'lon':lon,'lat':lat,'time':time})
     out1.to_netcdf(path_head+os.sep+f"\\swvl30\\swvl30_{year}.nc")
     print(year)
+
 
 
 
